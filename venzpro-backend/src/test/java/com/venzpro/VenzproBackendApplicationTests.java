e856771dedf60c95066f552c05bec9dde33b30ed
Eduardo <142151838+eduardoo-ely@users.noreply.github.com>
2026-03-19 18:26:16 +0000

    Feat: Criando o Backend

---
+++ venzpro-backend/src/test/java/com/venzpro/VenzproBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venzpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VenzproBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
